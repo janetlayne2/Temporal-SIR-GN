94403ef6c0d3e69c1445e8ceb1cbe62e415a8798
Janet Layne <97315350+janetlayne2@users.noreply.github.com>
2023-03-06 12:51:26 -0700

    Add files via upload

---
+++ directed_loader.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+
+class directed_loader:
+    
+    def __init__(self):
+        self.countID=0
+        self.G={}
+        self.co={}
+        self.revco={}
+    
+    def nodeID(self,x):
+        if x not in self.co:
+            self.co[x]=self.countID
+            self.countID=self.countID+1
+            self.revco[self.co[x]]=x
+        return self.co[x]
+    
+    def read(self,file):
+        x=file.values
+        for a in range(x.shape[0]):
+            i=self.nodeID(x[a,0])
+            j=self.nodeID(x[a,1])
+            self.addEdge((i,j,float(x[a,3])))
+        self.fixG()
+        
+    def storeEmb(self,file,data):
+        file1 = open(file, 'w') 
+        for a in range(data.shape[0]):
+            s=''+str(int(self.revco[a]))
+            for b in range(data.shape[1]):
+                s+=' '+str(data[a,b])
+            file1.write(s+"\n")
+        file1.close()
+            
+    
+    def fixG(self):
+        for g in range(len(self.G)):
+            orderSet=[t for t in self.G[g]]
+            orderSet.sort(reverse=True)
+            self.G[g]=[(t,np.array([x for x in self.G[g][t]['in']]),np.array([y for y in self.G[g][t]['out']])) for t in orderSet]
+
+            
+    def addEdge(self,s):
+        (l1,l2,t)=s
+        if l1 not in self.G:
+            self.G[l1]={}
+        if l2 not in self.G:
+            self.G[l2]={}
+        if t not in self.G[l1]:
+            self.G[l1][t]={}
+            self.G[l1][t]['out']=set()
+            self.G[l1][t]['in']=set()
+        if t not in self.G[l2]:
+            self.G[l2][t]={}
+            self.G[l2][t]['out']=set()
+            self.G[l2][t]['in']=set()
+        self.G[l1][t]['out'].add(l2)
+        self.G[l2][t]['in'].add(l1)
